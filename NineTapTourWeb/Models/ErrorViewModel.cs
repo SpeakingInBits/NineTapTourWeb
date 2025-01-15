a9ac9b83cefd0933cd643523eb5497152258edc1
JoeProgrammer88 <JoeProgrammer88@users.noreply.github.com>
2025-01-15 09:34:26 -0800

    Add ASP.NET Core MVC with Individual Accounts template

---
+++ NineTapTourWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NineTapTourWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
