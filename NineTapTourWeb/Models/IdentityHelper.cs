6642034f5fa20a89c443ce8b97524ee680f8bf88
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2025-01-16 15:09:47 -0800

    Refactor identity configuration in Program.cs
    Updated Program.cs to use IdentityHelper for identity options configuration. Added a new class, IdentityHelper, to centralize and manage identity settings such as password requirements, lockout settings, and user options, improving code organization and maintainability.

---
+++ NineTapTourWeb/Models/IdentityHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NineTapTourWeb.Models;
+
+public static class IdentityHelper
+{
+    internal static void ConfigureIdentityOptions(IdentityOptions options)
+    {
+        // Password settings
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredLength = 8;
+        options.Password.RequiredUniqueChars = 1;
+
+        // Lockout settings
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+        options.Lockout.MaxFailedAccessAttempts = 5;
+        options.Lockout.AllowedForNewUsers = true;
+
+        // User settings
+        options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+        options.User.RequireUniqueEmail = false;
+
+        // SignIn settings
+        options.SignIn.RequireConfirmedEmail = false;
+        options.SignIn.RequireConfirmedPhoneNumber = false;
+        options.SignIn.RequireConfirmedAccount = false;
+    }
+}
+
